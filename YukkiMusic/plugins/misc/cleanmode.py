135f0ff2fd545b8288290efcc60a2d504011cd87
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2023-07-20 12:35:55 +0700

    Update cleanmode.py

--- YukkiMusic/plugins/misc/cleanmode.py
+++ YukkiMusic/plugins/misc/cleanmode.py
@@ -18,7 +18,7 @@ import config
 from config import adminlist, chatstats, clean, userstats
 from strings import get_command
 from YukkiMusic import app, userbot
-from YukkiMusic.misc import SUDOERS
+from YukkiMusic.misc import OWNER_ID
 from YukkiMusic.utils.database import (get_active_chats,
                                        get_authuser_names, get_client,
                                        get_particular_top,
@@ -66,7 +66,7 @@ async def clean_mode(client, update, users, chats):
     await set_queries(1)
 
 
-@app.on_message(filters.command(BROADCAST_COMMAND) & SUDOERS)
+@app.on_message(filters.command(BROADCAST_COMMAND) & OWNER_ID)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
