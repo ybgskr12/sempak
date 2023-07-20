3bef2f6b0a445d68d94275ec0f61520c5bfecae2
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2023-07-20 12:43:29 +0700

    Update active

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -48,6 +48,12 @@ async def activevc(_, message: Message):
             disable_web_page_preview=True,
         )
 
+@app.on_message(filters.command("active", [".", "^", "-", "!", "/"]) & SUDOERS)
+async def activecilik(_, message: Message):
+    ms = len(await get_active_chats())
+    vd = len(await get_active_video_chats())
+    await app.send_message(message.chat.id, 
+        f"💽 Active Chats:**\n\n• **Music: {ms}`\n•Video:{vd}`")
 
 @app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
 async def activevi_(_, message: Message):
