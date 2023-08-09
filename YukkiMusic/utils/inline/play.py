4ce8df6e767c2a24d403d77fb56c9eca9a4ae195
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2023-08-09 20:09:15 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -17,7 +17,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_3"],  switch_inline_query_current_chat=""
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/suka2bagas",
+                text=_["S_B_9"], url=f"https://t.me/YourAmitty",
             ),
         ],
         [
@@ -69,10 +69,10 @@ def stream_markup(_, videoid):
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/suka2bagas",
+                text=_["S_B_9"], url=f"https://t.me/YourAmitty",
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/ybgskr17",
+                text=_["S_B_9"], url=f"https://t.me/allfucek",
             ),
         ],
         [
